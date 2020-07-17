f7c8c0dd48b552789d4cca3f7df954564fb70f2b
Simanta Paul <simanta.paul.26@gmail.com>
2020-07-18 03:43:16 +0600

    DishDetail Component

--- src/data/dishes.js
+++ src/data/dishes.js
@@ -6,7 +6,7 @@ const DISHES = [
         category: 'meal',
         label: 'Hot',
         price: '199',
-        description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+        description: 'This chicken burger gets an Asian-flavor twist with ginger and garlic. It\'s topped off with a fabulous coleslaw. If you like, serve the coleslaw as a side for grilled items, like chicken breasts, fish fillets or chops',
         comments: [
             {
                 id: 0,
@@ -47,12 +47,12 @@ const DISHES = [
     },
     {
         id: 1,
-        name: 'Chicken Bar B Q Pizza',
+        name: 'BBQ Chicken Pizza',
         image: 'assets/images/1pizza.jpg',
         category: 'meal',
         label: '',
         price: '599',
-        description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+        description: 'BBQ chicken pizza with tangy BBQ sauce, chicken, and red onion. Gouda cheese gives the homemade pizza the most delicious smoky flavor and cilantro adds a touch of freshness. My pizza dough recipe makes enough for two 12-inch pizzas, so you can freeze half for later. It’s an easy thick-style pizza crust with unbeatable flavor.',
         comments: [
             {
                 id: 0,
@@ -98,7 +98,7 @@ const DISHES = [
         category: 'appetizer',
         label: 'New',
         price: '120',
-        description: 'A quintessential ConFusion experience, is it a vada or is it a donut?',
+        description: 'These are deep-fried, very thin, salted slices of potato that are usually served at room temperature. French fries have numerous variants, from thick-cut to shoestring, crinkle, curly and many other names.',
         comments: [
             {
                 id: 0,
@@ -144,7 +144,7 @@ const DISHES = [
         category: 'dessert',
         label: '',
         price: '170',
-        description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+        description: 'A delectable, semi-sweet New York Style Cheese Cake, with cracker crust and spiced with Indian cardamoms',
         comments: [
             {
                 id: 0,
