3c04ac0ab682cb7367fb05b47428fb8c942df5a7
Simanta Paul <simanta.paul.26@gmail.com>
2020-08-01 19:09:21 +0600

    Redux Advanced

--- src/components/body/Home.js
+++ src/components/body/Home.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Loading from './Loading';
 
 class Home extends Component {
     render() {
         document.title = "Bohubrihi Restaurant";
         return (
             <div>
-
+                <Loading />
             </div>
         );
     }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Reducer } from './reducer';
-
-const myStore = createStore(Reducer);
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+const myStore = createStore(Reducer, applyMiddleware(logger, thunk));
 
 export default myStore; 
